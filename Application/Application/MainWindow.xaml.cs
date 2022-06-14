19df5a3994f81ca03dd4b05aa1aba0b7158911ac
Aurélien Cahet <aurelien.cahet@etu.univ-smb.fr>
2022-06-14 17:44:16 +0200

    ihm nouveu

--- Application/Application/MainWindow.xaml.cs
+++ Application/Application/MainWindow.xaml.cs
@@ -37,16 +37,36 @@ namespace Application
             newAutorisaition.date = calendrier.SelectedDate.Value.Date.ToShortDateString(); ;
             newAutorisaition.commentaire = commentaire.Text;
 
-            newAutorisaition.Create();
+
+            newAutorisaition.libelleMaladie = ((Maladie)listViewMaladie.SelectedItem).libelleMaladie;
+            newAutorisaition.nomMedicament = ((Medicament)listViewMedicament.SelectedItem).libelleMedicament;
+            MessageBoxResult result = MessageBox.Show($"Ajout de l'autorisation : \n maladie : {newAutorisaition.libelleMaladie}\n medicament : {newAutorisaition.nomMedicament}\n date :{newAutorisaition.date}\n commentaire : {newAutorisaition.commentaire}", "Valider", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            switch(result)
+
+            {
+                case MessageBoxResult.Yes:
+                    newAutorisaition.Create();
+                    break;
+
+
+                case MessageBoxResult.No:
+                    break;
+
+            }
+                
             ApplicationData.loadApplicationData();
             dataGridAuto.ItemsSource = ApplicationData.listeAutorisations;
         }
 
         private void DelAutorisation(object sender, RoutedEventArgs e)
         {
+
             Autorisation auto = (Autorisation)dataGridAuto.SelectedItem;
             auto.Delete();
 
+            ApplicationData.loadApplicationData();
+            dataGridAuto.ItemsSource = ApplicationData.listeAutorisations;
 
         }
 
