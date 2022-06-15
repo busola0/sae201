e03eb5211202cee19b6e34e0c0684843324cc2ed
Aurélien Cahet <aurelien.cahet@etu.univ-smb.fr>
2022-06-15 10:22:27 +0200

    IHM ++

--- Application/Application/MainWindow.xaml.cs
+++ Application/Application/MainWindow.xaml.cs
@@ -32,14 +32,16 @@ namespace Application
         private void AjoutAutorisation(object sender, RoutedEventArgs e)
         {
             Autorisation newAutorisaition = new Autorisation();
-            newAutorisaition.idMaladie= ((Maladie)listViewMaladie.SelectedValue).numero;
+            newAutorisaition.idMaladie = ((Maladie)listViewMaladie.SelectedValue).numero;
             newAutorisaition.idMedicament = ((Medicament)listViewMedicament.SelectedValue).numero;
             newAutorisaition.date = calendrier.SelectedDate.Value.Date.ToShortDateString(); ;
             newAutorisaition.commentaire = commentaire.Text;
-
+            
 
             newAutorisaition.libelleMaladie = ((Maladie)listViewMaladie.SelectedItem).libelleMaladie;
             newAutorisaition.nomMedicament = ((Medicament)listViewMedicament.SelectedItem).libelleMedicament;
+
+
             MessageBoxResult result = MessageBox.Show($"Ajout de l'autorisation : \n maladie : {newAutorisaition.libelleMaladie}\n medicament : {newAutorisaition.nomMedicament}\n date :{newAutorisaition.date}\n commentaire : {newAutorisaition.commentaire}", "Valider", MessageBoxButton.YesNo, MessageBoxImage.Question);
 
             switch(result)
@@ -63,7 +65,23 @@ namespace Application
         {
 
             Autorisation auto = (Autorisation)dataGridAuto.SelectedItem;
-            auto.Delete();
+            
+
+            MessageBoxResult result = MessageBox.Show($"Supprimer de l'autorisation : \n maladie : {auto.libelleMaladie}\n medicament : {auto.nomMedicament}\n date :{auto.date}\n commentaire : {auto.commentaire}", "Valider", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+
+            switch (result)
+
+            {
+                case MessageBoxResult.Yes:
+                    auto.Delete();
+                    break;
+
+
+                case MessageBoxResult.No:
+                    break;
+
+            }
+
 
             ApplicationData.loadApplicationData();
             dataGridAuto.ItemsSource = ApplicationData.listeAutorisations;
